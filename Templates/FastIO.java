26261b873b664d36957fa6799ffdfca429fdb2f9
Riya Saxena <riusaxena789@gmail.com>
2026-08-31 22:06:54 +0530

    Template for java solutions

---
+++ Templates/FastIO.java
@@ -0,0 +1,68 @@
+package Templates;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class FastIO {
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = 1;
+        // Uncomment if problem has test cases:
+        // t = scanner.nextInt();
+
+        while (t-- > 0) {
+            solve(scanner, out);
+        }
+
+        out.flush(); // Always flush at the end!
+    }
+
+    private static void solve(FastScanner in, PrintWriter out) {
+        // Write your solution here
+    }
+}
